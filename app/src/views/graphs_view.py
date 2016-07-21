a6d7e9fe23aeedf91b012a4c92c8fb1f80feddb3
dajiang <huaigang.zhao@outlook.com>
2016-07-21 10:21:13 +0800

    数据绑定图表
    从数据库查出数据绑定图表数据源

--- app/src/views/graphs_view.py
+++ app/src/views/graphs_view.py
@@ -3,7 +3,9 @@ from sqlalchemy import create_engine,func
 from . import views
 from .jdCamera_view import JDCamera
 from sqlalchemy.orm import sessionmaker
-engine = create_engine('mysql://root:123456@localhost/datacollection?charset=utf8')
+import json
+
+engine = create_engine('mysql://root:123456@localhost/cameraSalesDB?charset=utf8')
 # engine = create_engine('mysql://root:123456@localhost/datacollection',connect_args={'charset':'utf8'})
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -12,7 +14,16 @@ session = Session()
 @views.route('/graphs', methods=['GET', 'POST'])
 def graphs():
 
-    data = session.query(JDCamera.brand, func.sum(JDCamera.comment)).group_by(JDCamera.brand).order_by(func.sum(JDCamera.comment).desc()).all
+    data = session.query(JDCamera.brand, func.sum(JDCamera.comment)).group_by(JDCamera.brand).order_by(func.sum(JDCamera.comment).desc())
+    brands = []
+    counts = []
+    # print data
+    for d in data:
 
-    print data
-    return render_template('graphs.html', data=data)
+        # print d[1]
+        # print d[0]
+        brands.append(d[0])
+        counts.append(float(d[1]))
+
+    # print brands,counts
+    return render_template('graphs.html', brands=json.dumps(brands), counts=counts)
